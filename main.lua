b49fd1891de494981154dce2c647464d5c3da6a8
khang <trongkhangn384@gmail.com>
2026-02-07 19:15:12 +0700

    Add initial game loader script

---
+++ main.lua
@@ -0,0 +1,13 @@
+-- Game Do Min - Loader Test
+-- Repo: Game-d-m-n
+
+print("Game Do Min loaded successfully!")
+
+-- Ví dụ code test
+if game then
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Dò Mìn",
+        Text = "Load thành công!",
+        Duration = 5
+    })
+end
